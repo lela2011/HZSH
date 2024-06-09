2b978c915a4875819248a79e040b724cefdfe958
Jakob Mostert <jakob.mostert@outlook.com>
2024-06-09 18:49:07 +0200

    locally host jquery to comply with dsgvo

--- resources/views/components/iframe-layout.blade.php
+++ resources/views/components/iframe-layout.blade.php
@@ -6,8 +6,7 @@
         <meta name="theme-color" content="#ffffff">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>IPZ Master</title>
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="{{ asset('jquery/jquery-3.7.1.min.js') }}"></script>
         <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
         <link rel="stylesheet" href="https://www.uzh.ch/static/magnolia/assets/css/main.css" />
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
         <link rel="icon" type="image/x-icon" href="{{asset('images/favicon.ico')}}">
         <link rel="stylesheet" href="https://www.uzh.ch/static/magnolia/assets/css/main.css"/>
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="{{ asset('jquery/jquery-3.7.1.min.js') }}"></script>
         <x-tinymce-config/>
     </head>
     <body class="template-st04">
