46110f179fe0f8c4f4a79dafec79e27a85ab467b
Jakob Mostert <jakob.mostert@outlook.com>
2024-06-10 00:59:05 +0200

    updated forceRootUrl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        URL::forceRootUrl(Config::get('app.url'));
     }
 }
