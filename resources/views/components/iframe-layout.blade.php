79f65b8b3982781fba09a22ab52524fd48660363
Jakob Mostert <jakob.mostert@outlook.com>
2024-06-10 17:56:36 +0200

    changed iframe title to HZSH IPZ

--- resources/views/components/iframe-layout.blade.php
+++ resources/views/components/iframe-layout.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="theme-color" content="#ffffff">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>IPZ Master</title>
+        <title>HZSH IPZ</title>
         <script src="{{ asset('jquery/jquery-3.7.1.min.js') }}"></script>
         <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
         <link rel="stylesheet" href="https://www.uzh.ch/static/magnolia/assets/css/main.css" />
