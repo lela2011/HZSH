3bac7afb7ca1c8642bee5b536a50889a073bc3bc
Jakob Mostert <jakob.mostert@outlook.com>
2024-07-29 08:50:02 +0200

    Revert back to dynamic style update

--- resources/views/components/iframe-layout.blade.php
+++ resources/views/components/iframe-layout.blade.php
@@ -8,7 +8,7 @@
         <title>HZSH IPZ</title>
         <script src="{{ asset('jquery/jquery-3.7.1.min.js') }}"></script>
         <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
-        <link rel="stylesheet" href="https://www.uzh.ch/static/magnolia/768/assets/css/main.css" /> <!-- https://www.uzh.ch/static/magnolia/assets/css/main.css -->
+        <link rel="stylesheet" href="https://www.uzh.ch/static/magnolia/assets/css/main.css" />
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     </head>
     <body class="iframe-body">
